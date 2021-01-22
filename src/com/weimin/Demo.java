1ead89f3064d4c2f5d7444cd3e65413582d89ac4
weimin <2561394933@qq.com>
2021-01-22 14:12:25 +0800

    ten

--- src/com/weimin/Demo.java
+++ src/com/weimin/Demo.java
@@ -11,31 +11,31 @@ public class Demo {
         System.out.println(test2(40));
         long end = System.currentTimeMillis();
 
-        System.out.println(end-start);
+        System.out.println(end - start);
     }
 
     // 递归
-    public static int test(int n){
-        if(n==1||n==2){
+    public static int test(int n) {
+        if (n == 1 || n == 2) {
             return n;
         }
 
 
-        return test(n-2)+test(n-1);
+        return test(n - 2) + test(n - 1);
     }
 
 
     // 迭代
-    public static int test2(int n){
-        if(n==1||n==2){
+    public static int test2(int n) {
+        if (n == 1 || n == 2) {
             return n;
         }
 
         int one = 1;
         int two = 2;
         int sum = 0;
-        for (int i = 3; i <=n ; i++) {
-            sum = one+two;
+        for (int i = 3; i <= n; i++) {
+            sum = one + two;
             one = two;
             two = sum;
         }
